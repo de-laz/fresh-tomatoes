1be40a9ea87bcf32a17a73d8cc368795b4f4eaf7
Lazola Sifuba <sifubalazola@gmail.com>
2016-03-19 13:33:14 +0200

    Added movie year and IMDB rating

---
+++ src/models.py
@@ -0,0 +1,11 @@
+class Movie:
+    """
+
+    """
+
+    def __init__(self, title, poster_image_url, trail_youtube_url, imdb_rating, year):
+        self.title = title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trail_youtube_url
+        self.imdb_rating = imdb_rating
+        self.year = year
